9e58a9b1bae7805b58f8a08ac82e0152a050d634
WONG ZHENG QUAN <38060298+wongzhengquan@users.noreply.github.com>
2020-08-23 16:50:41 +0800

    MERN IG #30: Comments backend API

--- server/models/post.js
+++ server/models/post.js
@@ -6,7 +6,8 @@ const postSchema = mongoose.Schema({
   body: { type: String, required: true },
   imgUrl: { type: String, required: true },
   postedBy: { type: ObjectId, ref: "User" },
-  likes: [{ type: ObjectId, ref: "User" }]
+  likes: [{ type: ObjectId, ref: "User" }],
+  comments: [{ text: String, postedBy: { type: ObjectId, ref: "User" } }]
 })
 
 mongoose.model("Post", postSchema)
--- server/routes/post.js
+++ server/routes/post.js
@@ -69,4 +69,22 @@ router.put('/unlike', requireLogin, (req, res) => {
   })
 })
 
+router.put('/comment', requireLogin, (req, res) => {
+  const comment = { text: req.body.text, postedBy: req.user._id }
+
+  Post.findByIdAndUpdate(req.body.postId, {
+      $pull: { comments: comment }
+    }, {
+      new: true
+    })
+    .populate("comments.postedBy", "_id name")
+    .exec((err, result) => {
+      if (err) {
+        return res.status(422).json({ error: err })
+      }
+
+      res.json(result)
+    })
+})
+
 module.exports = router
