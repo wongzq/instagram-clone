c73d7baeada7cf6b3f6f58f657f2a01624131a43
WONG ZHENG QUAN <38060298+wongzhengquan@users.noreply.github.com>
2020-08-24 17:22:07 +0800

    MERN IG #25: Follow Unfollow API

--- server/routes/user.js
+++ server/routes/user.js
@@ -17,4 +17,32 @@ router.get('/users/:id', requireLogin, (req, res) => {
     .catch(_ => res.status(404).json({ error: "User not found" }))
 })
 
+router.put('/follow', requireLogin, (req, res) => {
+  User.findByIdAndUpdate(req.body.followeeId, {
+    $push: { followers: req.user._id }
+  }, { new: true }, (err) => {
+    if (err) return res.status(422).json({ error: err });
+
+    User.findByIdAndUpdate(req.user._id, {
+        $push: { following: req.body.followeeId }
+      }, { new: true })
+      .then(result => result.json())
+      .catch(err => res.status(422).json({ error: err }))
+  })
+})
+
+router.put('/unfollow', requireLogin, (req, res) => {
+  User.findByIdAndUpdate(req.body.unfolloweeId, {
+    $pull: { followers: req.user._id }
+  }, { new: true }, (err) => {
+    if (err) return res.status(422).json({ error: err });
+
+    User.findByIdAndUpdate(req.user._id, {
+        $pull: { following: req.body.followeeId }
+      }, { new: true })
+      .then(result => result.json())
+      .catch(err => res.status(422).json({ error: err }))
+  })
+})
+
 module.exports = router
