0b684defa556428550f5376683453a6ea46b9e03
WONG ZHENG QUAN <38060298+wongzhengquan@users.noreply.github.com>
2020-09-09 22:02:19 +0800

    Updated favicon.ico and index.html title

--- server/models/post.js
+++ server/models/post.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const { ObjectId } = mongoose.Schema.Types
 
 const postSchema = mongoose.Schema({
-  title: { type: String, required: true },
   body: { type: String, required: true },
   imgUrl: { type: String, required: true },
   postedBy: { type: ObjectId, ref: "User" },
